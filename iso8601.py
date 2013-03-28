08d6b9a6151d5d451b6c58e33fcef272b6717e65
Anders Hovmöller <boxed@killingar.net>
2013-03-28 08:38:17 +0100

    can't parse 00:00 times, which also resulted in timezones of +00:00 failing, fixes #1

--- iso8601.py
+++ iso8601.py
@@ -67,7 +67,7 @@ def parse_date(s):
         result = date(int(m.groupdict()['year']), int(m.groupdict()['month']), int(m.groupdict()['day']))
 
     # week dates
-    if not result:
+    if result is None:
         if 'W' in s:
             formats = [
                 # suffix for parsing, format, description
@@ -87,7 +87,7 @@ def parse_date(s):
 
     # ordinal dates
     description = 'YYYY-DDD'
-    if not result and len(s) >= len(description):
+    if result is None and len(s) >= len(description):
         try:
             result = datetime.strptime(s[:len(description)], '%Y-%j').date()
             s = s[:len(description)]
@@ -95,7 +95,7 @@ def parse_date(s):
             pass
 
     description = 'YYYYDDD'
-    if not result and len(s) >= len(description):
+    if result is None and len(s) >= len(description):
         try:
             result = datetime.strptime(s[:len(description)], '%Y%j').date()
             s = s[:len(description)]
@@ -103,53 +103,54 @@ def parse_date(s):
             pass
 
     # YYYY-MM # month only
-    if not result:
+    if result is None:
         m = re.match(r'^(?P<year>\d{4})-?(?P<month>\d{2})', s)
         if m:
             s = s[m.end():]
             result = date(int(m.groupdict()['year']), int(m.groupdict()['month']), 1)
 
-    if not result:
+    if result is None:
         description = 'YYYY'
         result = datetime.strptime(s[:len(description)], '%Y').date()
         s = s[:len(description)]
 
     return result
 
-def parse_time(s, timezone=None):
+def parse_time(in_s, timezone=None):
     result = None
     micros = 0
+    s = in_s
     if '.' in s:
         s, micros = s.split('.')
         s, micros = s+micros[6:], int(micros[:6])
 
     # hh:mm:ss
     # hhmmss
-    if not result:
+    if result is None:
         m = re.match(r'^(?P<hour>\d{2}):?(?P<minute>\d{2}):?(?P<second>\d{2})', s)
         if m:
             s = s[m.end():]
             result = time(int(m.groupdict()['hour']), int(m.groupdict()['minute']), int(m.groupdict()['second']), micros, timezone)
     # hh:mm
     # hhmm
-    if not result:
+    if result is None:
         m = re.match(r'^(?P<hour>\d{2}):?(?P<minute>\d{2})', s)
         if m:
             s = s[m.end():]
             result = time(int(m.groupdict()['hour']), int(m.groupdict()['minute']), 0, micros, timezone)
     # hh
-    if not result:
+    if result is None:
         m = re.match(r'^(?P<hour>\d{2})', s)
         if m:
             s = s[m.end():]
             result = time(int(m.groupdict()['hour']), 0, 0, micros, timezone)
 
-    if result:
+    if result is not None:
         if s:
             result = result.replace(tzinfo=parse_timezone(s))
         return result
 
-    raise Exception('Could not parse "%s" as a time' % s)
+    raise Exception('Could not parse "%s" as a time' % in_s)
 
 
 class Test(unittest.TestCase):
@@ -170,6 +171,7 @@ class Test(unittest.TestCase):
         self.assertEquals(parse('2012007'), date(2012, 1, 7))
 
         # times
+        self.assertEquals(parse('00:00'), time(0, 0))
         self.assertEquals(parse('12:04:23'), time(12, 4, 23))
         self.assertEquals(parse('120423'), time(12, 4, 23))
         self.assertEquals(parse('12:04'), time(12, 4, 0))
@@ -186,6 +188,7 @@ class Test(unittest.TestCase):
         self.assertEquals(parse('2008-09-03T20:56:35.450686+0100'), datetime(2008, 9, 3, 20, 56, 35, 450686, TimeZone(timedelta(minutes=60))))
         self.assertEquals(parse('2008-09-03T20:56:35.450686+01:30'), datetime(2008, 9, 3, 20, 56, 35, 450686, TimeZone(timedelta(minutes=60+30))))
         self.assertEquals(parse('2008-09-03T20:56:35.450686-01:30'), datetime(2008, 9, 3, 20, 56, 35, 450686, TimeZone(timedelta(minutes=-(60+30)))))
+        self.assertEqual(parse('2013-03-28T02:30:24+00:00'), datetime(2013, 3, 28, 2, 30, 24, tzinfo=TimeZone(timedelta(minutes=0))))
 
 if __name__ == '__main__':
     unittest.main()
